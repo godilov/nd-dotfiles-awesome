8cbf76dfd0960e6a1b75e712b25c62dd944b5b68
German Odilov <german.odilov@outlook.com>
2023-06-01 22:23:33 +0600

    nd.awesome: almost done

--- src/nd/awesome/core/interface.lua
+++ src/nd/awesome/core/interface.lua
@@ -20,36 +20,51 @@ fn = function(key, val)
 end
 
 return {
-    container = {
-        arcchart          = fn('container', container.arcchart),
-        background        = fn('container', container.background),
-        constraint        = fn('container', container.constraint),
-        margin            = fn('container', container.margin),
-        mirror            = fn('container', container.mirror),
-        only_on_screen    = fn('container', container.only_on_screen),
-        place             = fn('container', container.place),
-        radialprogressbar = fn('container', container.radialprogressbar),
-        rotate            = fn('container', container.rotate),
-        scroll            = fn('container', container.scroll),
-    },
-    layout = {
-        align  = fn('layout', layout.align),
-        fixed  = fn('layout', layout.fixed),
-        flex   = fn('layout', layout.flex),
-        grid   = fn('layout', layout.grid),
-        manual = fn('layout', layout.manual),
-        ratio  = fn('layout', layout.ratio),
-        stack  = fn('layout', layout.stack),
-    },
-    widget = {
-        calendar    = fn('widget', widget.calendar),
-        checkbox    = fn('widget', widget.checkbox),
-        graph       = fn('widget', widget.graph),
-        imagebox    = fn('widget', widget.imagebox),
-        piechart    = fn('widget', widget.piechart),
-        progressbar = fn('widget', widget.progressbar),
-        separator   = fn('widget', widget.separator),
-        slider      = fn('widget', widget.slider),
-        textbox     = fn('widget', widget.textbox),
+    raw = {
+        container = {
+            arcchart          = fn('container', container.arcchart),
+            background        = fn('container', container.background),
+            constraint        = fn('container', container.constraint),
+            margin            = fn('container', container.margin),
+            mirror            = fn('container', container.mirror),
+            place             = fn('container', container.place),
+            radialprogressbar = fn('container', container.radialprogressbar),
+            rotate            = fn('container', container.rotate),
+            scroll            = fn('container', container.scroll),
+        },
+        layout = {
+            align  = {
+                horizontal = fn('layout', layout.align.horizontal),
+                vertical   = fn('layout', layout.align.vertical),
+            },
+            fixed  = {
+                horizontal = fn('layout', layout.fixed.horizontal),
+                vertical   = fn('layout', layout.fixed.vertical),
+            },
+            flex   = {
+                horizontal = fn('layout', layout.flex.horizontal),
+                vertical   = fn('layout', layout.flex.vertical),
+            },
+            grid   = fn('layout', layout.grid),
+            manual = fn('layout', layout.manual),
+            ratio  = {
+                horizontal = fn('layout', layout.ratio.horizontal),
+                vertical   = fn('layout', layout.ratio.vertical),
+            },
+            stack  = fn('layout', layout.stack),
+        },
+        widget = {
+            calendar    = fn('widget', widget.calendar),
+            checkbox    = fn('widget', widget.checkbox),
+            graph       = fn('widget', widget.graph),
+            imagebox    = fn('widget', widget.imagebox),
+            piechart    = fn('widget', widget.piechart),
+            progressbar = fn('widget', widget.progressbar),
+            separator   = fn('widget', widget.separator),
+            slider      = fn('widget', widget.slider),
+            systray     = fn('widget', widget.systray),
+            textbox     = fn('widget', widget.textbox),
+            textclock   = fn('widget', widget.textclock),
+        },
     },
 }
--- src/nd/awesome/init.lua
+++ src/nd/awesome/init.lua
@@ -1,18 +1,182 @@
-local str_lib       = require 'nd.lib.core.str'
-local cache_lib     = require 'nd.lib.core.cache.fs'
+local fn_lib          = require 'nd.lib.core.fn'
+local str_lib         = require 'nd.lib.core.str'
+local type_lib        = require 'nd.lib.core.type'
+local cache_lib       = require 'nd.lib.core.cache.fs'
 
-local key_config    = require 'nd.awesome.config.key'
-local color_config  = require 'nd.awesome.config.color'
+local key_cache_res   = require 'nd.res.key.cache'
+local color_cache_res = require 'nd.res.color.cache'
 
-local navigation_fn = require 'nd.awesome.navigation'
-local appearance_fn = require 'nd.awesome.appearance'
-local proc          = require 'nd.awesome.proc'
+local key_config      = require 'nd.awesome.config.key'
+local color_config    = require 'nd.awesome.config.color'
 
-local concat2s      = str_lib.concat2s
+local interface       = require 'nd.awesome.core.interface'
 
-local filesystem    = require 'gears.filesystem'
+local key_fn          = require 'nd.awesome.core.key'
 
-local is_init       = false
+local proc            = require 'nd.awesome.proc'
+
+local iv              = fn_lib.iv
+local each            = fn_lib.each
+
+local concat2s        = str_lib.concat2s
+
+local is_str          = type_lib.is_str
+local is_fn           = type_lib.is_fn
+
+local key_scheme_fn   = key_cache_res.get_awesome
+local color_scheme_fn = color_cache_res.get_awesome
+
+local background      = interface.raw.container.background
+local align           = interface.raw.layout.align
+
+local awesome         = awesome
+local screen          = screen
+local client          = client
+local root            = root
+local awful           = require 'awful'
+local beautiful       = require 'beautiful'
+local gears           = require 'gears'
+local wibox           = require 'wibox'
+
+local filesystem      = gears.filesystem
+
+local is_init         = false
+
+
+local set_wallpapers      = nil
+local set_tags_properties = nil
+local set_tags            = nil
+local set_screen          = nil
+
+
+set_wallpapers = function(s, wp)
+    gears.wallpaper.maximized(is_str(wp) and wp or is_fn(wp) and wp(s))
+end
+
+set_tags_properties = function(index, s)
+    return {
+        screen = s,
+        selected = index == 1,
+        layout = awful.layout.suit.spiral,
+    }
+end
+
+set_tags = function(s, tags)
+    each(function(elem)
+        local ind = elem[1]
+        local val = elem[2]
+
+        awful.tag.add(val[1], val[2](ind, s))
+    end, iv(tags))
+end
+
+set_screen = function(key_scheme)
+    return function(s)
+        set_wallpapers(s, beautiful.wallpaper)
+        set_tags(s, {
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+            { '-', set_tags_properties },
+        })
+
+        local wibar      = awful.wibar {
+            screen  = s,
+            visible = false,
+        }
+
+        local taglist    = awful.wibox.taglist
+            {
+                screen          = s,
+                filter          = awful.widget.taglist.filter.all,
+                buttons         = key_scheme.button.taglist,
+
+                widget_template = {
+                    id     = 'background_role',
+                    widget = wibox.container.background,
+                    {
+                        id           = 'text_role',
+                        widget       = wibox.widget.textbox,
+                        valign       = 'center',
+                        align        = 'center',
+                        forced_width = 40,
+                    },
+                    create_callback = function(self, t, _, _)
+                        self:connect_signal('mouse::enter', function()
+                            if t ~= awful.screen.focused().selected_tag then
+                                self.bg = beautiful.bg_minimize
+                            end
+                        end)
+                        self:connect_signal('mouse::leave', function()
+                            if t ~= awful.screen.focused().selected_tag then
+                                self.bg = beautiful.bg_normal
+                            end
+                        end)
+                    end,
+                    update_callback = function(_, _, _, _)
+                    end,
+                },
+            }
+
+        local tasklist   = awful.wibox.tasklist {
+            screen          = s,
+            filter          = awful.widget.tasklist.filter.currenttags,
+            buttons         = key_scheme.button.tasklist,
+
+            layout          = {
+                layout = wibox.layout.fixed.horizontal,
+            },
+
+            widget_template = {
+                id     = 'background_role',
+                widget = wibox.container.background,
+                {
+                    widget = wibox.container.margin,
+                    top    = 6,
+                    bottom = 6,
+                    left   = 6,
+                    right  = 6,
+                    {
+                        id     = 'icon_role',
+                        widget = wibox.widget.imagebox,
+                    },
+                },
+                create_callback = function(self, c, _, _)
+                    self:connect_signal('mouse::enter', function()
+                        if c ~= client.focus and not c.minimized then
+                            self.bg = beautiful.bg_minimize
+                        end
+                    end)
+                    self:connect_signal('mouse::leave', function()
+                        if c ~= client.focus and not c.minimized then
+                            self.bg = beautiful.bg_normal
+                        end
+                    end)
+                end,
+                update_callback = function(_, _, _, _)
+                end,
+            },
+        }
+
+        local textclock_ = wibox.widget.textclock(' %d.%m.%Y %T ', 1)
+
+        wibar:setup {
+            background {
+                shape = gears.shape.rectangle,
+                bg    = beautiful.bg_normal,
+                align.horizontal {
+                    taglist,
+                    tasklist,
+                    textclock_,
+                },
+            },
+        }
+    end
+end
 
 return function()
     if not is_init then
@@ -23,12 +187,36 @@ return function()
             { 'picom',   '' },
         }
 
-        local config = {
-            key   = key_config['main'],
-            color = color_config['main'],
+        local key_scheme = key_scheme_fn(key_config['main'])
+        local color_scheme = color_scheme_fn(color_config['main'])
+
+        root.keys(key_fn(key_scheme.key.root, awful.key))
+        root.buttons(key_fn(key_scheme.button.root, awful.button))
+
+        beautiful.init(color_scheme.theme.raw)
+
+        awful.rules.rules = {
+            {
+                rule = {},
+                properties = {
+                    raise        = true,
+                    keys         = key_fn(key_scheme.key.client, awful.key),
+                    buttons      = key_fn(key_scheme.button.client, awful.button),
+                    border_width = beautiful.border_width,
+                    border_color = beautiful.border_normal,
+                    focus        = awful.client.focus.filter,
+                    screen       = awful.screen.preferred,
+                    placement    = awful.placement.no_overlap + awful.placement.no_offscreen,
+                },
+            },
         }
 
-        navigation_fn(config.key)
-        appearance_fn(config.color)
+        awesome.set_preferred_icon_size(256)
+
+        awful.screen.connect_for_each_screen(set_screen(key_scheme))
+
+        screen.connect_signal('property::geometry', set_wallpapers)
+
+        is_init = true
     end
 end
